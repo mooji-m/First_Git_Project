945526e85144d4392f7f32062bf346d0a497f636
mooji-m <droiddiver14@gmail.com>
2023-07-12 16:52:38 +0430

    i am in develop branch

--- app/src/main/java/com/mooji/cod/first_git_project/MainActivity.kt
+++ app/src/main/java/com/mooji/cod/first_git_project/MainActivity.kt
@@ -7,5 +7,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        android.widget.Toast.makeText(this, "iam in develop branch", Toast.LENGTH_SHORT).show()
     }
 }
