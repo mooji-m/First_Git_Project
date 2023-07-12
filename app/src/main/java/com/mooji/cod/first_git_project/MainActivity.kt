0701658219900c80a6cd82540ff89d683d16f59c
mooji-m <droiddiver14@gmail.com>
2023-07-12 17:22:31 +0430

    second commit

--- app/src/main/java/com/mooji/cod/first_git_project/MainActivity.kt
+++ app/src/main/java/com/mooji/cod/first_git_project/MainActivity.kt
@@ -7,5 +7,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+        Toast.makeText(this, "hello main branch", Toast.LENGTH_SHORT).show()
     }
+
 }
